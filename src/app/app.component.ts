aeaf3aadad1e7667386b98f82a53610c07b738aa
Denys Oliinyk <68949838+doliinyk@users.noreply.github.com>
2024-02-16 18:27:37 +0200

    Oliinyk / App ngOnInit (#4)
    * Add ngOnInit to App
    
    * Import fix
    
    * Workflow update
    
    * Fix workflow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  public ngOnInit(): void {}
+}
