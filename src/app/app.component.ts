2ebb9357ea5f45e7d9394410a1f5b5816b0fc612
doliinyk <denysoliinyk@outlook.com>
2024-02-16 18:15:05 +0200

    Import fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   public ngOnInit(): void {}
