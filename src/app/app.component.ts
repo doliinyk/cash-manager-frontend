8b27ec3d91b62941db3b861f531d260284304cd1
Kazumen <yura07042004@gmail.com>
2024-02-16 14:43:42 +0200

    Removed log from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import {environment} from "../environments/environment";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-
-  constructor() {
-    console.log(environment)
-  }
-}
+export class AppComponent {}
