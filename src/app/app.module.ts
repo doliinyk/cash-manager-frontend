0bb25ce782ea0fac3b44a04f90f826bd1e5f1693
Yurii Romanchak <71885527+Kazumen@users.noreply.github.com>
2024-04-04 15:12:43 +0300

    Added ngxs and service for localization (#14)
    * Added ngxs and service for localization
    
    * Changed style with prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
 
 import { MaterialModule } from 'shared/modules/material.module';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -28,9 +32,13 @@ import { NavComponent } from './nav/nav.component';
     AppRoutingModule,
     FlexLayoutModule,
     HttpClientModule,
-    MaterialModule
+    MaterialModule,
+    NgxsModule.forRoot([]),
+    NgxsLoggerPluginModule.forRoot({ disabled: environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot({ disabled: environment.production })
   ],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
