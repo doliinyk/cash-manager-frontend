e0230486a8cb438b12a8c41f847a55721c0e91bf
witolDark <witalikspelina@gmail.com>
2024-04-05 14:17:37 +0300

    Nav fixes

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import {CommonModule} from '@angular/common';
-import {NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import {MaterialModule} from 'shared/modules/material.module';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {TranslateModule} from "@ngx-translate/core";
-import {FlexLayoutModule} from "@angular/flex-layout";
+import { MaterialModule } from 'shared/modules/material.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { TranslateModule } from "@ngx-translate/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
