ce27f5f44f347f650d16a3820b58342fca13011e
witolDark <witalikspelina@gmail.com>
2024-04-05 14:24:19 +0300

    Prettier

--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from 'shared/modules/material.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { TranslateModule } from "@ngx-translate/core";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { TranslateModule } from '@ngx-translate/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
   exports: [LoginComponent, RegisterComponent],
-  imports: [
-    CommonModule,
-    MaterialModule,
-    TranslateModule,
-    FlexLayoutModule
-  ]
+  imports: [CommonModule, MaterialModule, TranslateModule, FlexLayoutModule]
 })
-export class AuthModule {
-}
+export class AuthModule {}
