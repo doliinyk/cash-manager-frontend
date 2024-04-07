3d2b0aee56060ccbd8351b37bd087920ef88295a
Kazumen <yura07042004@gmail.com>
2024-04-07 13:21:29 +0300

    fixed tests

--- src/app/core/auth/email-activation/email-activation.component.spec.ts
+++ src/app/core/auth/email-activation/email-activation.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { MaterialModule } from 'shared/modules/material.module';
 
 import { EmailActivationComponent } from './email-activation.component';
@@ -12,7 +13,7 @@ describe('EmailActivationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EmailActivationComponent],
-      imports: [RouterTestingModule.withRoutes([]), HttpClientModule, MaterialModule]
+      imports: [RouterTestingModule.withRoutes([]), HttpClientModule, MaterialModule, TranslateModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(EmailActivationComponent);
