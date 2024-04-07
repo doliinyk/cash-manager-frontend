cc4aad581af2478ce5b21be2f3545c709ecd4639
Kazumen <yura07042004@gmail.com>
2024-04-07 13:16:44 +0300

    fixed build

--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 export class AuthService {
   constructor(private httpClient: HttpClient) {}
 
-  public verifyEmail(userId: string, activationToken: string): Observable<void> {
-    const params = new HttpParams().set('userId', userId).set('activationToken', activationToken);
+  public verifyEmail(userId: string | null, activationToken: string | null): Observable<void> {
+    const params = new HttpParams().set('userId', userId ? userId : 'null').set('activationToken', activationToken ? activationToken : 'null');
     return this.httpClient.post<void>('http://localhost:8080/api/v1/auth/activate', {}, { params: params });
   }
 }
